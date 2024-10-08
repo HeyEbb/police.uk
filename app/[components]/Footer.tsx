ddb5ed7a011a1f42f1f9e66d85b67d9ff5dab94e
Ellis Collinson <elliscollinson@me.com>
2024-10-08 12:49:10 +0100

    💄 Add sider for smaller screens

--- app/[components]/Footer.tsx
+++ app/[components]/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <div className="flex justify-center items-center h-16 bg-gray-800 text-white mt-10">
-                <p>&copy; 2024 Ellis Collinson</p>
+                <p>&copy; 2024 HeyEbb</p>
             </div>
         </footer>
     )
--- app/[components]/Header.tsx
+++ app/[components]/Header.tsx
@@ -1,26 +1,86 @@
+"use client";
+
+import { useState } from "react";
 import Logo from "../src/img/puk-logo.png";
 import Image from "next/image";
+import { LuMenu, LuX } from "react-icons/lu";
+
+// styles
+import header from "../sass/header/header.module.scss";
 
 export default function Header() {
+  /* --------------------------------- states --------------------------------- */
+  const [siderOpen, setSiderOpen] = useState(false);
+
+  const navigationData = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "About",
+      link: "/about",
+    },
+    {
+      id: 3,
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+
+  /* --------------------------------- markup --------------------------------- */
   return (
     <>
       {/* container */}
       <section className="w-full bg-blue-600 flex justify-center items-center px-5 h-16">
-        <span className="core-container bg-green-500 flex justify-between ">
-          <span>UK Police Scanner</span>
-          {/* <Image
+        <span className="core-container flex justify-between items-center">
+          <Image
             className="p-3"
             src={Logo}
             alt="logo"
             width={100}
             height={100}
-          /> */}
-          <nav className="flex gap-4 ">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-          </nav>
+          />
+          <span className="flex">
+            <LuMenu
+              className={header.icon}
+              onClick={() => setSiderOpen(!siderOpen)}
+            />
+            <nav className={header.navigation}>
+              {navigationData.map((item) => (
+                <a key={item.id} href={item.link}>
+                  {item.name}
+                </a>
+              ))}
+            </nav>
+          </span>
         </span>
       </section>
+
+      {/* sidebar */}
+      {siderOpen && (
+        <section className="bg-gray-800 absolute left-0 h-[100vh] w-80">
+          {/* header of siderbar */}
+          <div className=" w-full flex justify-between p-3">
+            <h3 className="text-white mb-0">Sidebar title here</h3>
+            <LuX
+              className={header.closeIcon}
+              onClick={() => setSiderOpen(false)}
+            />
+          </div>
+          <hr/>
+          {/* pages */}
+          <nav className="flex flex-col m-3 gap-1">
+            {navigationData.map((item) => (
+              <a key={item.id} href={item.link} className={header.mobileItem}>
+                {item.name}
+              </a>
+            ))}
+          </nav>
+        </section>
+      )}
     </>
   );
 }
